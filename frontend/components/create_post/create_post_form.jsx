2f7ebcf28ee7e53b175337c7eef9cf7bc90d84db
mvodkin <mvodkin@gmail.com>
2016-11-04 10:15:39 -0400

    remove extraneous margin from new post creation

--- frontend/components/create_post/create_post_form.jsx
+++ frontend/components/create_post/create_post_form.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Canvas from "./canvas";
 
 const CreatePostForm = (props) => {
-  
+
   return (
     <main className="new-post-form">
-      <h1>ADD PIXLS</h1>
+      <h1 className="new-post-heading">ADD PIXLS</h1>
       <Canvas createPost={props.createPost}/>
     </main>
   )
