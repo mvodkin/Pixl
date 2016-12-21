9191ca7f1422e2a47224aaf4de22f157731360f2
mvodkin <mvodkin@gmail.com>
2016-12-20 21:44:58 -0500

    add headroom feature

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,36 +1,60 @@
 import React from "react";
 import { Link, hashHistory } from "react-router";
+import Headroom from "react-headroom";
 
-const Header = ({logout, currentUser}) => {
+class Header extends React.Component {
 
-  const handleLogout = () => {
-    logout();
+  constructor(props, logout, currentUser) {
+    super(props);
+    this.state = {
+      headroomDisabled: false
+    };
+    this.logout = logout;
+    this.currentUser = currentUser;
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  handleLogout() {
+    this.logout();
     hashHistory.push("login");
   }
 
-  const profileLink = () => {
-    return `/user/${currentUser.id}`
+  profileLink() {
+    return `/user/${this.currentUser.id}`
   }
 
-  return (
+  componentWillReceiveProps(newProps) {
+    debugger
+    if (window.location.hash.includes("new")) {
+      this.setState({headroomDisabled: true});
+    } else {
+      this.setState({headroomDisabled: false});
+    }
+  }
 
-    <div className="header-nav">
-      <Link to="/" className="logo-nav" data-text="Pixl">Pixl</Link>
+  render() {
+    return (
 
-      <form className="search">
-        <input className="search-bar" type="text" placeholder="Search" />
-      </form>
+      <Headroom disable={this.state.headroomDisabled} className="headroom">
+        <div className="header-nav">
+          <Link to="/" className="logo-nav" data-text="Pixl">Pixl</Link>
 
-      <div className="nav-list-container">
-        <ul className="nav-list">
-          <li><Link to="/new" data-text="new post">draw</Link></li>
-          <li><Link to="/explore" data-text="notifications">explore</Link></li>
-          <li><Link to={profileLink()} data-text="profile">profile</Link></li>
-          <li><div onClick={handleLogout}>log out</div></li>
-        </ul>
-      </div>
-    </div>
+          <form className="search">
+            <input className="search-bar" type="text" placeholder="Search" />
+          </form>
 
-  );
+          <div className="nav-list-container">
+            <ul className="nav-list">
+              <li><Link to="/new" data-text="new post">draw</Link></li>
+              <li><Link to="/explore" data-text="notifications">explore</Link></li>
+              <li><Link to={this.profileLink()} data-text="profile">profile</Link></li>
+              <li><div onClick={this.handleLogout}>log out</div></li>
+            </ul>
+          </div>
+        </div>
+      </Headroom>
+
+    );
+  }
 };
 export default Header;
