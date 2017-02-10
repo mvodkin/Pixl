f4517b35587867e1b282082483a2c6c927492c4a
mvodkin <mvodkin@gmail.com>
2017-02-10 17:35:37 -0500

    Revert "Revert "fix bug where comments are created for wrong post after creating a new drawing""
    This reverts commit 0d1790fdaba9f0a4187d59c5bfb19d6a6927997e.

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,60 +1,36 @@
 import React from "react";
 import { Link, hashHistory } from "react-router";
-import Headroom from "react-headroom";
 
-class Header extends React.Component {
+const Header = ({logout, currentUser}) => {
 
-  constructor(props, logout, currentUser) {
-    super(props);
-    this.state = {
-      headroomDisabled: false
-    };
-    this.logout = logout;
-    this.currentUser = currentUser;
-    this.handleLogout = this.handleLogout.bind(this);
-  }
-
-  handleLogout() {
-    this.logout();
+  const handleLogout = () => {
+    logout();
     hashHistory.push("login");
   }
 
-  profileLink() {
-    return `/user/${this.currentUser.id}`
+  const profileLink = () => {
+    return `/user/${currentUser.id}`
   }
 
-  componentWillReceiveProps(newProps) {
-    debugger
-    if (window.location.hash.includes("new")) {
-      this.setState({headroomDisabled: true});
-    } else {
-      this.setState({headroomDisabled: false});
-    }
-  }
+  return (
 
-  render() {
-    return (
+    <div className="header-nav">
+      <Link to="/" className="logo-nav" data-text="Pixl">Pixl</Link>
 
-      <Headroom disable={this.state.headroomDisabled} className="headroom">
-        <div className="header-nav">
-          <Link to="/" className="logo-nav" data-text="Pixl">Pixl</Link>
+      <form className="search">
+        <input className="search-bar" type="text" placeholder="Search" />
+      </form>
 
-          <form className="search">
-            <input className="search-bar" type="text" placeholder="Search" />
-          </form>
+      <div className="nav-list-container">
+        <ul className="nav-list">
+          <li><Link to="/new" data-text="new post">draw</Link></li>
+          <li><Link to="/explore" data-text="notifications">explore</Link></li>
+          <li><Link to={profileLink()} data-text="profile">profile</Link></li>
+          <li><div onClick={handleLogout}>log out</div></li>
+        </ul>
+      </div>
+    </div>
 
-          <div className="nav-list-container">
-            <ul className="nav-list">
-              <li><Link to="/new" data-text="new post">draw</Link></li>
-              <li><Link to="/explore" data-text="notifications">explore</Link></li>
-              <li><Link to={this.profileLink()} data-text="profile">profile</Link></li>
-              <li><div onClick={this.handleLogout}>log out</div></li>
-            </ul>
-          </div>
-        </div>
-      </Headroom>
-
-    );
-  }
+  );
 };
 export default Header;
