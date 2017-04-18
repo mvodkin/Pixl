4e13f79d20fc00a8b3fa14f322ed6a3c31064a4b
mvodkin <mvodkin@gmail.com>
2017-04-17 22:21:54 -0400

    refactor like button to be functional component

--- frontend/components/posts/like_button.jsx
+++ frontend/components/posts/like_button.jsx
@@ -1,41 +1,30 @@
 import React from "react";
 
-class LikeButton extends React.Component {
+const LikeButton = (props) => {
 
-  constructor(props) {
-    super(props)
-
-    this.liked;
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    if (this.isLiked()) {
-      this.props.unlikePost(this.props.post_id.toString());
+  const handleClick = () => {
+    if (isLiked()) {
+      props.unlikePost(props.post_id.toString());
     } else {
-      this.props.likePost(this.props.post_id.toString());
+      props.likePost(props.post_id.toString());
     }
   }
 
-  buttonClass() {
-    return this.isLiked() ? "like-button unlike" : "like-button like"
+  const buttonClass = () => {
+    return isLiked() ? "like-button unlike" : "like-button like"
   }
 
-  isLiked() {
-    const liked = this.props.likers.findIndex((liker) => {
-      return liker.id === this.props.currentUser.id
+  const isLiked = () => {
+    const liked = props.likers.findIndex((liker) => {
+      return liker.id === props.currentUser.id
     });
     return liked > -1;
   }
 
-  render() {
-
-    return (
-      <div className={this.buttonClass()} onClick={this.handleClick}>
-      </div>
-    );
-
-  }
+  return (
+    <div className={buttonClass()} onClick={handleClick}>
+    </div>
+  );
 
 }
 
